7b4e89f25c39c9d1e0125fd838b235374589ac06
zxy423 <78031547+zxy423@users.noreply.github.com>
2021-02-08 11:22:18 +0000

    Create Role.java

---
+++ src/Role.java
@@ -0,0 +1,25 @@
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+    private String roleName;
+
+    private String userName;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+}
